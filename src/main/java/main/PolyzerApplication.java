142c00615527745475c9b5c9f4c78a47efae642b
aottolini <aottolini@agro.uba.ar>
2021-02-18 10:56:40 -0300

    project rename

--- src/main/java/main/PolyzerApplication.java
+++ src/main/java/main/PolyzerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BioSeqApplication {
+public class PolyzerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BioSeqApplication.class, args);
+		SpringApplication.run(PolyzerApplication.class, args);
 	}
 
 }
--- src/test/java/main/PolyzerApplicationTests.java
+++ src/test/java/main/PolyzerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BioSeqApplicationTests {
+class PolyzerApplicationTests {
 
 	@Test
 	void contextLoads() {
